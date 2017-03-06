f122d99512d38b0369bac9bfeea1a4afc0d40eb2
Mariia Dobberstein <Mariia_Dobberstein@epam.com>
2017-03-06 12:23:44 +0100

    first version of factorial function

---
+++ FactorialClass.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class FactorialClass {
+    int fact(int n)
+    {
+        int result;
+
+        if(n==1)
+            return 1;
+
+        result = fact(n-1) * n;
+        return result;
+    }
+}
+
+
+
